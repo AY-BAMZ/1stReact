923c8369a8db09f80d89066df18c657b0b96aa0e
AY-BAMZ <aybamz181@gmail.com>
2020-07-06 12:18:25 +0100

    Updates

---
+++ precache-manifest.3951197b4aa041f205de9575b53df06f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23168f1bc32152d0c2e66596e56bb869",
+    "url": "/AY-BAMZ/1stReact.git/index.html"
+  },
+  {
+    "revision": "0bd37b95747aea73a319",
+    "url": "/AY-BAMZ/1stReact.git/static/css/main.4c098369.chunk.css"
+  },
+  {
+    "revision": "b49cfb777b2d8a4386ec",
+    "url": "/AY-BAMZ/1stReact.git/static/js/2.5ab6f3df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AY-BAMZ/1stReact.git/static/js/2.5ab6f3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd37b95747aea73a319",
+    "url": "/AY-BAMZ/1stReact.git/static/js/main.fa24ba1c.chunk.js"
+  },
+  {
+    "revision": "a803a9346f574d3d4541",
+    "url": "/AY-BAMZ/1stReact.git/static/js/runtime-main.10545d15.js"
+  },
+  {
+    "revision": "5cbf62501a0516f96e3149912f5cd434",
+    "url": "/AY-BAMZ/1stReact.git/static/media/4.5cbf6250.JPG"
+  }
+]);
